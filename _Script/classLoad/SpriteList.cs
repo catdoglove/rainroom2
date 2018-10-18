9bafbd8ad87e64600fae451deb41546e51ef8e34
BORAme <naktop88@gmail.com>
2018-10-18 17:38:25 +0900

    spriteList
    cs

---
+++ _Script/classLoad/SpriteList.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+[CreateAssetMenu(fileName="창문")]
+public class SpriteList : ScriptableObject {
+
+	public int 창문;
+
+
+
+}
