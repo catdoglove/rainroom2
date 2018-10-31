9f13fab27beb24ecea9d5778949050fc04c65b3f
catdoglove <34932357+catdoglove@users.noreply.github.com>
2018-10-31 14:45:34 +0900

    광고
    ㅈㄱㄴ

---
+++ _Script/AdmobADS.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class AdmobADS : MonoBehaviour {
+
+    //배너
+    private BannerView bannerView;
+    AdRequest request;
+
+    //영상
+    private RewardBasedVideoAd rewardBasedVideo;
+    string adUnitIdvideo;
+    
+    int rewardCoin;
+    
+
+    // Use this for initialization
+    void Start () {
+        
+
+#if UNITY_ANDROID
+            string appId = "ca-app-pub-3940256099942544~3347511713";
+#elif UNITY_IPHONE
+            string appId = "ca-app-pub-3940256099942544~1458002511";
+#else
+        string appId = "unexpected_platform";
+#endif
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(appId);
+
+        //this.RequestBanner();
+        
+
+        rewardBasedVideo = RewardBasedVideoAd.Instance;
+        
+        // Called when the user should be rewarded for watching a video.
+        rewardBasedVideo.OnAdRewarded += HandleRewardBasedVideoRewarded;
+        // Called when the ad is closed.
+        rewardBasedVideo.OnAdClosed += HandleRewardBasedVideoClosed;
+
+        RequestRewardedVideo();
+    }
+
+
+    private void RequestBanner()
+    {
+#if UNITY_ANDROID
+            string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+#elif UNITY_IPHONE
+            string adUnitId = "ca-app-pub-3940256099942544/2934735716";
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+
+        // Create a 320x50 banner at the top of the screen.
+        bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+
+        // Load the banner with the request.
+        bannerView.LoadAd(request);
+    }
+
+
+    
+
+    private void RequestRewardedVideo()
+    {
+
+#if UNITY_ANDROID
+            adUnitIdvideo = "ca-app-pub-3940256099942544/5224354917";
+#elif UNITY_IPHONE
+            adUnitIdvideo = "ca-app-pub-3940256099942544/1712485313";
+#else
+        adUnitIdvideo = "unexpected_platform";
+#endif
+        // Create an empty ad request.
+        request = new AdRequest.Builder().Build();
+        // Load the rewarded video ad with the request.
+        rewardBasedVideo.LoadAd(request, adUnitIdvideo);
+    }
+
+    //시청보상
+    public void HandleRewardBasedVideoRewarded(object sender, Reward args)
+    {        
+
+    }
+
+    //동영상닫음
+    private void HandleRewardBasedVideoClosed(object sender, System.EventArgs args)
+    {
+        RequestRewardedVideo();
+    }
+
+    public void showAdmobVideo()
+    {
+        if (rewardBasedVideo.IsLoaded())
+        {
+            rewardBasedVideo.Show();
+        }
+        else
+        {
+
+        }
+    }
+    
+
+
+    public void callBanner()
+    {
+        this.RequestBanner();
+    }
+
+}
---
+++ _Script/UnityADS.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class UnityADS : MonoBehaviour {
+
+    private string gameId = "1486550";//★테스트ID , Window > Services 설정 테스트 바꿀것 test 1486550
+    public int soundck;
+
+	int sG,mG;
+
+    // Use this for initialization
+    void Start () {
+		//StartCoroutine ("adTimeFlow");
+          if (Advertisement.isSupported)
+          {
+              Advertisement.Initialize(gameId, true);
+          }
+      }
+      
+    // Update is called once per frame
+
+    public void ShowRewardedAd()
+    {
+        if (Advertisement.IsReady("rewardedVideo"))
+        {
+            ShowOptions options = new ShowOptions { resultCallback = HandleShowResult };
+            Advertisement.Show("rewardedVideo", options);
+        }
+        else
+        {
+        }
+    }
+    
+
+    private void HandleShowResult(ShowResult result)
+    {
+        if (result == ShowResult.Finished)
+        {
+        }
+    }
+
+    
+
+	IEnumerator adTimeFlow(){
+		while (mG>-1) {
+
+			sG = PlayerPrefs.GetInt("secf",300);
+			mG = (int)(sG / 60);
+			sG = sG-(sG / 60)*60;
+			if (sG < 0) {
+				sG = 0;
+				mG = 0;
+				//goPy.SetActive (true);
+			} else {
+			}
+			sG = PlayerPrefs.GetInt("secf",300);
+			sG = sG - 1;
+			if (sG < 0) {
+				sG = -1;
+			}
+			PlayerPrefs.SetInt("secf",sG);
+			yield return new WaitForSeconds(1f);
+		}
+	}
+}
