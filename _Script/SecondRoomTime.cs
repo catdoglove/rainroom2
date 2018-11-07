2d488976e8650482a5bd154f6a86094b9f3359f1
catdoglove <34932357+catdoglove@users.noreply.github.com>
2018-11-07 16:49:35 +0900

    씨앗물주기 코딩

---
+++ _Script/SecondRoomTime.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondRoomTime : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        //업데이트대신쓴다
+        StartCoroutine("UpdateSec");
+    }
+
+    /// <summary>
+    /// 1초당변경하는것들
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator UpdateSec()
+    {
+        int a = 0;
+        while (a == 0)
+        {
+            
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+
+}
---
+++ _Script/SeedTime.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SeedTime : MonoBehaviour {
+
+    public int seed_i,seedWater_i, coldRain_i;
+    public int[] seedCPrice_i;
+
+    string lastTime;
+
+    public GameObject seedWindow_obj, seedYetWindow_obj;
+    public GameObject seedImg_obj;
+    public Sprite[] seed_spr;
+
+    int minute;
+    int hours;
+
+    // Use this for initialization
+    void Start () {
+        SeedTimeFlow();
+
+    }
+
+
+    void SeedTimeFlow()
+    {
+
+        seed_i = PlayerPrefs.GetInt("seed", 0);
+        seedWater_i = PlayerPrefs.GetInt("seedWater", 0);
+        System.DateTime d = new System.DateTime(1970, 1, 1, 0, 0, 0, System.DateTimeKind.Utc);
+        lastTime = PlayerPrefs.GetString("seedLastTime", d.ToString());
+        System.DateTime lastDateTime = System.DateTime.Parse(lastTime);
+        System.TimeSpan compareTime = System.DateTime.Now - lastDateTime;
+        hours = (int)compareTime.TotalHours;
+        minute = (int)compareTime.TotalMinutes;
+        minute = minute - (minute / 60) * 60;
+        minute = 59 - minute;
+        hours = 1 - hours;
+        if (hours < 0)
+        {
+            if (seedWater_i > seed_i)
+            {
+                seed_i = PlayerPrefs.GetInt("seed", 0);
+                seed_i++;
+                PlayerPrefs.SetInt("seed", 0);
+                //seedImg_obj.GetComponent<Image>().sprite = seed_spr[seed_i];
+                PlayerPrefs.Save();
+            }
+        }
+        
+    }
+
+    public void TouchSeed()
+    {
+        SeedTimeFlow();
+        if (hours < 0)
+        {
+            if (seedWater_i == seed_i)
+            {
+                seedWindow_obj.SetActive(true);
+            }
+            else {
+                seedYetWindow_obj.SetActive(true);
+            }
+            
+        }
+        else
+        {
+            seedYetWindow_obj.SetActive(true);
+        }
+    }
+
+    public void CloseSeed()
+    {
+        seedWindow_obj.SetActive(false);
+        seedYetWindow_obj.SetActive(false);
+    }
+
+    public void SeedYes()
+    {
+        string str = PlayerPrefs.GetString("code", "");
+        coldRain_i = PlayerPrefs.GetInt(str + "c", 0);
+
+        if (coldRain_i >= seedCPrice_i[seed_i])
+        {
+            coldRain_i = coldRain_i - seedCPrice_i[seed_i];
+            PlayerPrefs.SetInt(str + "c", coldRain_i);
+
+            PlayerPrefs.SetString("seedLastTime", System.DateTime.Now.ToString());
+            seedWater_i = PlayerPrefs.GetInt("seedWater", 0);
+            seedWater_i++;
+            PlayerPrefs.SetInt("seedWater", 0);
+            PlayerPrefs.Save();
+        }
+        else
+        {
+            //물부족캄
+        }
+    }
+
+
+
+}
