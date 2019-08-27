ed484cb89a80ab52234121efd1a2f6378c020177
catdoglove <catdoglove@users.noreply.github.com>
2019-08-27 12:39:08 +0900

    사운드 수정

---
+++ _Script/SoundHandlerRain.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundHandlerRain : MonoBehaviour {
+    
+    public AudioSource BGS;
+    float BGSVol_f;
+
+    public GameObject audio_obj; 
+
+	// Use this for initialization
+	void Start () {
+
+        OnLoadSound();
+        BGS.time = 5f;
+        BGS.Play();
+    }
+	
+    
+
+
+    // Update is called once per frame
+    void Update () {
+        OnLoadSound();
+    }
+    
+
+    public void OnLoadSound()
+    {
+        BGSVol_f = PlayerPrefs.GetFloat("bgs", 1f);
+        BGS.volume = BGSVol_f;
+    }
+}
