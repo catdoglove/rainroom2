ce95147d956693ffbe8e7e2c34c541602de0f66a
catdoglove <catdoglove@users.noreply.github.com>
2020-02-06 16:13:47 +0900

    표지판 문방구 상점창 배치
    상점 기능 코딩 완료
    연동 코딩중

--- _Script/NoteShop.cs
+++ _Script/NoteShop.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class NoteShop : MonoBehaviour {
 
     public GameObject noteShopWindow_obj, noteShopBuyYN_obj, noteShopHelp_obj;
-    public GameObject noteShopSign_obj, noteToast_obj;
+    public GameObject noteShopSign_obj, noteToast_obj, noteNum_obj;
     Color color;
     string str;
 
@@ -14,6 +14,7 @@ public class NoteShop : MonoBehaviour {
 
     public int noteShopNum_i;
 
+    public Text name_txt;
     // Use this for initialization
     void Start () {
 
@@ -24,6 +25,11 @@ public class NoteShop : MonoBehaviour {
     //문방구 열기
     public void ActNoteShop()
     {
+        //노트 몇권가지고 있나?
+        if (PlayerPrefs.GetInt("havenote", 0)>=2)
+        {
+
+        }
         if (noteShopWindow_obj.activeSelf == true)
         {
             noteShopWindow_obj.SetActive(false);
@@ -34,22 +40,28 @@ public class NoteShop : MonoBehaviour {
         }
     }
     //노트
-    public void ButNote()
+    public void BuyNote()
     {
         noteShopNum_i = 0;
+        priceShop_i = 10;
+        name_txt.text = "공책";
         noteShopBuyYN_obj.SetActive(true);
     }
     //지우개
-    public void ButEraser()
+    public void BuyEraser()
     {
         noteShopNum_i = 1;
+        priceShop_i = 2;
+        name_txt.text = "지우개";
         noteShopBuyYN_obj.SetActive(true);
     }
 
     //연필
-    public void ButPencil()
+    public void BuyPencil()
     {
         noteShopNum_i = 2;
+        priceShop_i = 2;
+        name_txt.text = "필기구";
         noteShopBuyYN_obj.SetActive(true);
     }
 
@@ -73,7 +85,13 @@ public class NoteShop : MonoBehaviour {
         priceShop_i = 0;
         if (spade >= priceShop_i)
         {
-
+            spade = spade - priceShop_i;
+            PlayerPrefs.SetInt(str + "sd", spade);
+        }
+        else
+        {
+            StopCoroutine("needSpadeFadeOut");
+            StartCoroutine("needSpadeFadeOut");
         }
         noteShopBuyYN_obj.SetActive(false);
     }
@@ -85,7 +103,7 @@ public class NoteShop : MonoBehaviour {
 
 
     //스페이드없다
-    IEnumerator toastPencleImgFadeOut()
+    IEnumerator needSpadeFadeOut()
     {
         color.a = Mathf.Lerp(0f, 1f, 1f);
         noteToast_obj.GetComponent<Image>().color = color;
