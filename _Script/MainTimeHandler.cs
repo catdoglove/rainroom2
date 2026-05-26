04d831b3b9a841e74857b51729f18337c790073f
love <34932357+catdoglove@users.noreply.github.com>
2026-05-26 14:47:58 +0900

    앱게시용 시간 갱신

--- _Script/MainTimeHandler.cs
+++ _Script/MainTimeHandler.cs
@@ -22,7 +22,7 @@ public class MainTimeHandler : MonoBehaviour {
     // Use this for initialization
     void Start () {
 
-        if (PlayerPrefs.GetInt("emergencyCODE4", 0) == 0)
+        if (PlayerPrefs.GetInt("emergencyCODE5", 0) == 0)
         {
 
 
@@ -67,7 +67,7 @@ public class MainTimeHandler : MonoBehaviour {
 
 
 
-            PlayerPrefs.SetInt("emergencyCODE4", 99);
+            PlayerPrefs.SetInt("emergencyCODE5", 99);
         }
 
             //빗물
